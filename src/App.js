7465a38c8ce31a6fcfcee00ae4346f0d1b74fe87
rodude123 <rodude123@gmail.com>
2020-06-25 11:06:30 +0100

    added broswer router and main functionality

--- src/App.js
+++ src/App.js
@@ -1,58 +1,66 @@
-import React, { Component } from 'react'
-import ListContacts from './ListContacts'
-import * as ContactsAPI from './utils/ContactsAPI'
-import CreateContact from './CreateContact'
-import { Route } from 'react-router-dom'
+import React, {Component} from 'react';
+import ListContacts from "./ListContacts";
+import * as cpi from "./utils/ContactsAPI"
+import CreateContact from "./CreateContact";
 
-class App extends Component {
-  state = {
-    contacts: []
-  }
-  componentDidMount() {
-    ContactsAPI.getAll()
-      .then((contacts) => {
-        this.setState(() => ({
-          contacts
-        }))
-      })
-  }
-  removeContact = (contact) => {
-    this.setState((currentState) => ({
-      contacts: currentState.contacts.filter((c) => {
-        return c.id !== contact.id
-      })
-    }))
 
-    ContactsAPI.remove(contact)
-  }
-  createContact = (contact) => {
-    ContactsAPI.create(contact)
-      .then((contact) => {
-        this.setState((currentState) => ({
-          contacts: currentState.contacts.concat([contact])
-        }))
-      })
-  }
-  render() {
-    return (
-      <div>
-        <Route exact path='/' render={() => (
-          <ListContacts
-            contacts={this.state.contacts}
-            onDeleteContact={this.removeContact}
-          />
-        )} />
-        <Route path='/create' render={({ history }) => (
-          <CreateContact
-            onCreateContact={(contact) => {
-              this.createContact(contact)
-              history.push('/')
-            }}
-          />
-        )} />
-      </div>
-    )
-  }
+class App extends Component
+{
+	state = {
+		contacts: [],
+		screen: "list"
+	}
+	
+	componentDidMount()
+	{
+		cpi.getAll().then(contacts =>
+		{
+			this.setState(() =>
+			({
+				contacts
+			}))
+		})
+	}
+	
+	removeContact = (contact) =>
+	{
+		this.setState(currentSate =>
+		({
+			contacts: currentSate.contacts.filter((c) =>
+			{
+				return c.id !== contact.id
+			})
+		}))
+		cpi.remove(contact)
+	}
+	
+	render()
+	{
+		return (
+			<div>
+				{
+					this.state.screen === "list" &&
+					(
+						<ListContacts contacts={this.state.contacts}
+						              onDeleteContact={this.removeContact}
+						              onNavigate={() =>
+						              {
+						              	this.setState(() =>
+						                ({
+							                screen: "create"
+						                }))
+						              }}/>
+					)
+				}
+				{
+					this.state.screen === "create" &&
+					(
+						<CreateContact/>
+					)
+				}
+			</div>
+		);
+	}
 }
 
-export default App
+export default App;
